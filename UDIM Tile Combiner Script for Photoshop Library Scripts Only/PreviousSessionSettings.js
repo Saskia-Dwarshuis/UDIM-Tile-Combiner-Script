599f0c5d9c227d85358e216a8f7c9b5eec146be2
Saskia Dwarshuis <83183389+Saskia-Dwarshuis@users.noreply.github.com>
2021-04-25 15:19:46 -0400

    Create PreviousSessionSettings.js

---
+++ UDIM Tile Combiner Script for Photoshop Library Scripts Only/PreviousSessionSettings.js
@@ -0,0 +1,16 @@
+function previousSessionRadioButtonSettings(){
+SingleTile.value = false
+SingleUDIMTileText.value = false
+UDIMTileRange.value = true
+SingleUDIMTile.text = "1001"
+FirstUDIMTile.text = "1001"
+LastUDIMTile.text = "1010"
+MaterialChannelEnterList.text = "BaseColor, Metallic, Roughness"
+FileSavePrefix.text = "Prefix_"
+FileSaveExtensionType.selection = 1
+FolderSelectionShortcutModeCheckbox.value = true
+ScriptCompletedAlertCheckbox.value = true
+IgnoreImageOverwritingWarningsCheckbox.value = false
+allTexturesFolder = ""
+outputFolder = ""
+}
