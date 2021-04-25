e036ff33fb3242e6ffbd34a3efc9f694bb76c2a9
Saskia Dwarshuis <83183389+Saskia-Dwarshuis@users.noreply.github.com>
2021-04-25 15:17:50 -0400

    Create ActiveScriptSettings.js

---
+++ UDIM Tile Combiner Script for Photoshop Library Scripts Only/ActiveScriptSettings.js
@@ -0,0 +1,6 @@
+function radioButtonSelected(){
+LoadDefaultButton.value = true
+LoadPreviousSessionButton.value = false
+UserDefinedPreferencesButton.value = false
+userSetSettingsFileToPointTo = ""
+}
